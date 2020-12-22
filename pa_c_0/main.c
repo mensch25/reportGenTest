ace57432c8c49d4b3c2b842a58c3a323e6cf8c65
mensch25 <mensch25@yandex.ru>
2020-12-23 04:28:45 +0600

    fixed this

--- pa_c_0/main.c
+++ pa_c_0/main.c
@@ -8,7 +8,6 @@ int main(int argc, char* argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
     if (ProcRank == 0) {
-        printf("\nHello from process %3d\n", ProcRank);
         for (int i = 1; i < ProcNum; i++) {
             MPI_Recv(&RecvRank, 1, MPI_INT, i,
                      MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
