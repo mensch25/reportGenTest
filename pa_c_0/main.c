1ce58d3e98395691f05aa39bd58e66e649a44326
mensch25 <mensch25@yandex.ru>
2020-12-24 02:35:20 +0600

    ya sdelal'

--- pa_c_0/main.c
+++ pa_c_0/main.c
@@ -8,6 +8,7 @@ int main(int argc, char* argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
     if (ProcRank == 0) {
+	// I fixed that
         for (int i = 1; i < ProcNum; i++) {
             MPI_Recv(&RecvRank, 1, MPI_INT, i,
                      MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
